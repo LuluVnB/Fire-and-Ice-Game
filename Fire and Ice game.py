07f2dcc254a47777edc99839cb25eff160ee132f
Gian <paraguya.gian@gmail.com>
2022-06-25 20:15:08 -0700

    Co-authored-by: kenketchup <kenketchup@users.noreply.github.com> Co-authored-by: BRUHnerd <BRUHnerd@users.noreply.github.com> Co-authored-by: Malarky-is-real <Malarky-is-real@users.noreply.github.com>

--- Fire and Ice game.py
+++ Fire and Ice game.py
@@ -34,19 +34,45 @@ player_pos = [200,450]  #where michael starts
 
 screen = tk.Tk()
 title = screen.title("Fire and Ice")
-canvas = Canvas(screen, width = 1000, height = 1000)
+canvas = Canvas(screen, width = 5000, height = 1000, bg = "#0e2415")
 canvas.grid()
 score = 0
 
+
+#ICE MICHAEL
+michael_Ice = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/characters/Michael_ice.gif", format="gif -index 2")
+michael_Ice = michael_Ice.subsample(2,2)
+canvas.create_image(player_pos, image = michael_Ice)
+
+#FIRE MICHAEL
+michael_fire = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/characters/Michael_fire.gif", format="gif -index 2")
+michael_fire = michael_fire.subsample(2,2)
+canvas.create_image(player_pos, image = michael_fire)
+
+
+
+
+#FIRE OBSTACLE
+fire = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/imgs/fire_obstacle.gif", format="gif -index 2")
+fire = fire.subsample(2,2)
+canvas.create_image(player_pos, image = fire)
+
+#ICE OBSTACLE
+ice = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/imgs/fire_obstacle.gif", format="gif -index 2")
+ice = ice.subsample(2,2)
+canvas.create_image(player_pos, image = ice)
+
+
+
+#BACKGROUND IMAGE
 background = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/imgs/caveBackground.png")
-background = background.subsample(3,3)
-canvas.create_image(10, 10, image=background, anchor=NW)
+background = background.subsample(2,3)
+canvas.create_image(0, 70, image=background, anchor=NW)
 
 
 
 
 #This is michael
-# michael = PhotoImage(file = "C:/Users/parag/Documents/Fire and Ice/characters/Michael.png")
 michaelWalking = PhotoImage(file="C:/Users/parag/Documents/Fire and Ice/characters/Michael_walking.gif", format="gif -index 2")
 michaelWalking = michaelWalking.subsample(2,2)
 canvas.create_image(player_pos, image = michaelWalking)
@@ -66,6 +92,14 @@ def shoot_after(): #moves the bullet
 
 #global pipe_id
 
+# def determine_pipes():
+#         pipe_color = random.randint(0,1)  
+#         if pipe_color == 0:
+#             canvas.create_rectangle(900, 0, 1000, 1000, fill = "red", tag = "colored_pipe1")
+#         else:
+#             canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe1")  
+#
+    
 def create_pipe1(): #makes pipe and randomly decides its color
     pipe_color = random.randint(0,1)  
     if pipe_color == 0:
@@ -73,11 +107,10 @@ def create_pipe1(): #makes pipe and randomly decides its color
     else:
         canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe1")
     screen.after(20, move_pipe1)
-    screen.after(randint(600,1000), create_pipe2)
+    screen.after(3000, create_pipe2)
 
 def move_pipe1():
-    speed = (-20)
-    canvas.move("colored_pipe1", -20, 0)
+    canvas.move("colored_pipe1", -10, 0)
     canvas.update()
     screen.after(20, move_pipe1)
 
@@ -88,38 +121,114 @@ def create_pipe2(): #makes pipe and randomly decides its color
     else:
         canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe2")
     screen.after(20, move_pipe2)
-    screen.after(randint(600,1000), create_pipe1)
+    screen.after(2500, create_pipe3)
 
 def move_pipe2():
-    speed = (-20)
-    canvas.move("colored_pipe2", speed, 0)
+    canvas.move("colored_pipe2", -10, 0)
     canvas.update()
     screen.after(20, move_pipe2)
 
+def create_pipe3(): #makes pipe and randomly decides its color
+    pipe_color = random.randint(0,1)  
+    if pipe_color == 0:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "red", tag = "colored_pipe3")
+    else:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe3")
+    screen.after(20, move_pipe3)
+    screen.after(1000, create_pipe4)
 
-def animation(count):
-    frames = info.n_frames
-    im2 = imgs[count]
-    count += 1
-    if count == frames:
-        count = 0   
+def move_pipe3():
+    canvas.move("colored_pipe3", -10, 0)
+    canvas.update()
+    screen.after(20, move_pipe3)
+
+def create_pipe4(): #makes pipe and randomly decides its color
+    pipe_color = random.randint(0,1)  
+    if pipe_color == 0:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "red", tag = "colored_pipe4")
+    else:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe4")
+    screen.after(20, move_pipe4)
+    screen.after(1000, create_pipe5)
+
+def move_pipe4():
+    canvas.move("colored_pipe4", -10, 0)
+    canvas.update()
+    screen.after(20, move_pipe4)
+
+def create_pipe5(): #makes pipe and randomly decides its color
+    pipe_color = random.randint(0,1)  
+    if pipe_color == 0:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "red", tag = "colored_pipe5")
+    else:
+        canvas.create_rectangle(900, 0, 1000, 1000, fill = "blue", tag = "colored_pipe5")
+    screen.after(20, move_pipe5)
+    screen.after(1000, create_pipe1)
+
+def move_pipe5():
+    canvas.move("colored_pipe5", -10, 0)
+    canvas.update()
+    screen.after(20, move_pipe5)
 
 
 
-   
-
-
-
-
-    canvas.itemconfig(develon, image=im2)
-    global anim
-
-    anim = root.after(100, lambda: animation(count))
+# def determine_pipes():
+#     pipe_color = random.randint(0,1)  
+#     if pipe_color == 0:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = fire, tag = "colored_pipe1")
+#     else:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = ice , tag = "colored_pipe1")  
+#     move_pipe():
     
-   
+# def move_pipe():
+#     x=1000
+#     while(x>200):
+#         canvas.move("colored_pipe1", -20, 0)
+#         x-=20
+#         canvas.update()
+#         screen.after(20, move_pipe)
+#     x=1000
+#     determine_pipes()
+
+
+# def create_pipe():
+
+#         screen.after(1000, create_pipe2)
+
+#         canvas.move("colored_pipe1", -20, 0)
+#         x=1000
+#         x-=-20
+#         canvas.update()
+#         screen.after(20, move_pipe1)
     
 
+# def create_pipe1(): #makes pipe and randomly decides its color
+#     pipe_color = random.randint(0,1)  
+#     if pipe_color == 0:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = fire, tag = "colored_pipe1")
+#     else:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = ice, tag = "colored_pipe1")
+#     screen.after(20, move_pipe1)
+#     screen.after(1000, create_pipe2)
 
+# def move_pipe1():
+#     canvas.move("colored_pipe1", -20, 0)
+#     canvas.update()
+#     screen.after(20, move_pipe1)
+
+# def create_pipe2(): #makes pipe and randomly decides its color
+#     pipe_color = random.randint(0,1)  
+#     if pipe_color == 0:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = fire, tag = "colored_pipe2")
+#     else:
+#         canvas.create_rectangle(900, 0, 1000, 1000, image = ice, tag = "colored_pipe2")
+#     screen.after(20, move_pipe2)
+#     screen.after(1000, create_pipe1)
+
+# def move_pipe2():
+#     canvas.move("colored_pipe2", -20, 0)
+#     canvas.update()
+#     screen.after(20, move_pipe2)
 
 pygame.init()
 
